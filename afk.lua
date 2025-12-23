730c446c94ec9371aadfd53b933942ebd05bf660
pp9999 <labidas9988@gmail.com>
2025-12-23 04:04:41 +0200

    update

--- afk.lua
+++ afk.lua
@@ -1,21 +1,26 @@
 local API = require("api")
-print("Run Lua script afker.")
-
+print("Run Lua port fish script afker.")
 
 while API.Read_LoopyLoop() do
-
     API.DoRandomEvents()
 
-
-    --API.KeyboardPress2(0x51, 40, 60) --Q
-    --API.KeyboardPress2(0x57, 40, 60) --W
-    --print(API.IsCacheLoaded())
-    --Inventory:InvItemcount({999})
-    print(#Inventory:ReadInvArrays33(true))
-    if API.Check_continue_Open() then
-        API.KeyboardPress31(0x20, 40, 100)
+    if API.WaitUntilMovingandAnimEnds(4,2) then
+        if API.Buffbar_GetIDstatus(51490,false).conv_text > 0 then
+            local obj = API.ReadAllObjectsArray({1}, {25220}, {})
+            if #obj > 0 then
+                API.DoAction_NPC__Direct(0x3c, API.OFF_ACT_InteractNPC_route, obj[1])
+                API.MarkTiles({obj[1].Tile_XYZ},0,0,2,false,false,WPOINT.new(0,0,0),WPOINT.new(0,0,0))
+                API.RandomSleep2(1600, 4300, 300)
+                print("Fishin")
+            end
+        end
     end
 
-  
-API.RandomSleep2(8600, 10300, 300)
+
+    print("Inventory:IsFull():" .. tostring(Inventory:IsFull()))
+    print("Inventory:IsEmpty():" .. tostring(Inventory:IsEmpty()))
+    print("Inventory:Invfreecount():" .. tostring(Inventory:Invfreecount()))
+    print("Inventory:Contains():" .. tostring(Inventory:Contains(44472)))
+
+API.RandomSleep2(600, 7300, 300)
 end
